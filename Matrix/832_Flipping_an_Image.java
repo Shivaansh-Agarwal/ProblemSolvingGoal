da953b56dff3572c56ce0b143e4e8afb3f35571b
Shivaansh Agarwal <shivaansh.agarwal@kony.com>
2019-08-26 11:45:58 +0530

    Code Improved

--- Matrix/832_Flipping_an_Image.java
+++ Matrix/832_Flipping_an_Image.java
@@ -1,20 +1,12 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] A) {
         int temp;
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[0].length/2;j++){
-                temp = A[i][j];
-                A[i][j] = A[i][A[0].length-1-j];
-                A[i][A[0].length-1-j] = temp;
-                
-            }
-        }
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[0].length;j++){
-                if(A[i][j]==1)
-                    A[i][j] = 0;
-                else
-                    A[i][j] = 1;
+        int noCol = A[0].length;
+        for(int[] row : A){
+            for(int j=0; j<(noCol+1)/2 ;j++){
+                temp = row[j] ^ 1;
+                row[j] = row[noCol-1-j] ^ 1;
+                row[noCol-1-j] = temp;
             }
         }
         return A;
