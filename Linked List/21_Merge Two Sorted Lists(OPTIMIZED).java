7800de3caa2231fb0f7fffd8f6c5a88df0b965ba
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2019-09-26 19:09:57 +0530

    21 Merge Two Sorted Lists(OPTIMIZED)

---
+++ Linked List/21_Merge Two Sorted Lists(OPTIMIZED).java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    ListNode head = null;
+    ListNode tail = null;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null && l2==null)
+            return null;
+        else if(l1==null && l2!=null)
+            return l2;
+        else if(l1!=null && l2==null)
+            return l1;
+        // For first element
+        if(l1.val<l2.val){
+            head = l1;
+            tail = l1;
+            l1 = l1.next;
+        }
+        else{
+            head = l2;
+            tail = l2;
+            l2 = l2.next;
+        }
+        //For remaining elements.
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                tail.next = l1;
+                tail = l1;
+                l1 = l1.next;
+            }
+            else{
+                tail.next = l2;
+                tail = l2;
+                l2 = l2.next;
+            }
+        }
+        if(l1!=null)
+            tail.next = l1;
+        else if(l2!=null)
+            tail.next = l2;
+        return head;
+    }
+}
