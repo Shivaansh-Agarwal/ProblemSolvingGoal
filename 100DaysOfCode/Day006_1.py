48b5cd5a260095b1809a04b1e48f9c23c96c6640
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2020-01-08 20:34:44 +0530

    Day 6

---
+++ 100DaysOfCode/Day006_1.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def plusOne(self, A):
+        s = [str(i) for i in A]
+        num = int("".join(s))
+        num+=1
+        ans = [int(i) for i in str(num)]
+        return ans
