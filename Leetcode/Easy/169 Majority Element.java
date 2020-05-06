c8fbcb6358222f6b5d0dbeec960e1dd62a6a1675
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2020-05-06 19:48:34 +0530

    Day-6 May Leetcoding Challenge

---
+++ Leetcode/Easy/169 Majority Element.java
@@ -0,0 +1,29 @@
+/*
+    169. Majority Element
+    https://leetcode.com/problems/majority-element/
+    06/05/2020
+    May LeetCoding Challenge
+    Day-6
+*/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> hmap = new HashMap<>();
+        for(int num : nums){
+            if(hmap.containsKey(num)){
+                hmap.put(num, hmap.get(num)+1);
+            } else {
+                hmap.put(num, 1);
+            }
+        }
+        int threshold = (int)Math.floor(nums.length/2);
+        int majorityElement = -1;
+        for(Integer key: hmap.keySet()){
+            if(hmap.get(key) > threshold){
+                majorityElement = key;
+                break;
+            }
+        }
+        return majorityElement;
+    }
+}
