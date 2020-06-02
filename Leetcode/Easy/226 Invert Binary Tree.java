dc7ef9d3b1441ad3ac9e68c844ba5904a5ff9f18
shivaansh.agarwal <shivaansh.agarwal@kony.com>
2020-06-02 11:54:56 +0530

    Day-1 June Leetcoding Challenge

---
+++ Leetcode/Easy/226 Invert Binary Tree.java
@@ -0,0 +1,39 @@
+/*
+	226. Invert Binary Tree
+	https://leetcode.com/problems/invert-binary-tree/
+	01/06/2020
+	June Leetcoding Challenge
+	Day-1
+
+	O(n) Time Complexity
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null)
+            return root;
+        
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        
+        root.left = right;
+        root.right = left;
+        
+        return root;
+    }
+}
